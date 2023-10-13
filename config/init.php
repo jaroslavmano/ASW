b498512328f71c4b00cb2bf11e0ad3acc9c06284
Jaroslav Maňo <mano@jaroslavmano.eu>
2023-10-13 20:56:59 +0200

    Add Basic version of Program

--- config/init.php
+++ config/init.php
@@ -1,10 +1,65 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Dokument bez názvu</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$nowPath = $_SERVER["DOCUMENT_ROOT"];
+
+define("db_prefix","asw_");
+define('web_url', "https://asware.local/");
+
+
+
+// DEFINICE DB
+$configFile = parse_ini_file('db_config.ini');
+
+define('DB_SERVER', $configFile['db_host']);
+define('DB_PORT', $configFile['db_port']);
+define('DB_USERNAME', $configFile['db_username']);
+define('DB_PASSWORD', $configFile['db_password']);
+define('DB_DATABASE', $configFile['db_database']);
+
+$configDiscordFile = parse_ini_file('discord_config.ini');
+
+define('OAUTH2_CLIENT_ID', $configDiscordFile['client_id']);
+define('OAUTH2_CLIENT_SECRET', $configDiscordFile['client_secret']);
+
+// NASTAVENÍ URL ADRES PRO DISCORD
+define('DISCORD_AUTHORIZE', "https://discordapp.com/api/oauth2/authorize");
+define('DISCORD_TOKEN', "https://discordapp.com/api/oauth2/token");
+define('DISCORD_REVOKE', "https://discordapp.com/api/oauth2/token/revoke");
+define('DISCORD_BASE', "https://discordapp.com/api/users/@me");
+define('DISCORD_REDIRECT', web_url."login/process.php");
+
+if( file_exists("./inc/php/class/")){
+// IMPORT DATABÁZOVÉ TŘÍDY
+    require_once("./inc/php/class/mysql.php");
+
+    $db = new SQL(DB_SERVER.":".DB_PORT,DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+
+// PŘIPOJENÍ SE K DB
+    $db->Connection();
+
+// IMPORT VŠECH DALŠÍCH TŘÍD
+    require_once("./inc/php/class/user.php");
+    require_once("./inc/language/czech.php");
+    require_once("./inc/php/class/log.php");
+    require_once("./inc/php/class/permissions.php");
+    require_once("./inc/php/class/groups.php");
+    require_once("./inc/php/class/modules.php");
+    require_once("./inc/php/class/system.php");
+    require_once("./inc/php/class/ranks.php");
+
+// VYTVOŘENÍ LOGOVOÁNÍ
+    $log = new Log();
+
+    $system = new system_class();
+    $system->GetSettings();
+    $system->GetMenu();
+
+    require_once("./inc/php/class/functions_help.php");
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,39 +1,71 @@
+<?php require("./config/init.php"); ?>
+<?php
+if(isset($_GET["discordID"]) || isset($_SESSION["ID"])){
+	if(isset($_GET["discordID"])){
+		$loginUser = new User('',$_GET["discordID"]);
+		if($loginUser->InfoUser() === false){
+			echo '<meta http-equiv="refresh" content="0;url=login.php">';
+		}
+		$_SESSION["ID"] = $loginUser->id; 
+	}
+	$LoginPermission = array();
+	$loginUser = new User($_SESSION["ID"]);
+	$loginUser->InfoUser(); 
+	$groups = $loginUser->UserGroups();
+	if(is_array($groups)){
+		foreach($groups as $group){
+			$groupClass = new Groups($group);
+			$LoginPermission = array_unique(array_merge($LoginPermission, $groupClass->GroupPermissions()));	
+		}
+	}else if (!empty($groups)){
+		$groupClass = new Groups($groups);
+		$LoginPermission = array_unique(array_merge($LoginPermission, $groupClass->GroupPermissions()));	
+	}
+} else {
+	echo '<meta http-equiv="refresh" content="0;url=login.php">';
+}
+?>
 <!doctype html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?=$system->SystemSettings["system_name"]?></title>
+	<meta name="author" content="Jaroslav Maňo">
+  	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://cdn.tailwindcss.com"></script>
-  <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-          }
-        }
-      }
-    }
-  </script>
+	  <script>
+		tailwind.config = {
+		  theme: {
+			extend: {
+			  colors: {
+				clifford: '#da373d',
+			  }
+			}
+		  }
+		}
+	  </script>
 	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
-<body class="font-mono text-white flex h-screen items-center justify-center  bg-cover bg-center bg-[url('./data/temp/Promo.2022-1.2.mov')] bg-blend-multiply">
-	<a href="https://thehiddenones.cz" class="absolute font-bold top-0 left-0 p-4 text-lg hover:text-black hover:cursor-pointer md:p-6 md:text-xl lg:p-8 lg:text-2xl"><ion-icon name="chevron-back-outline"></ion-icon> ZPĚT NA HLAVNÍ STRÁNKU</a>
-	<section class="mx-auto  w-[70%] md:w-[50%] lg:w-[40%]">
-		<header class="m-auto text-center content-center">
-			<img src="./data/temp/logo.png" class="rounded-full w-[100%] mx-auto">
-			<a class="bg-sky-500 rounded-md p-3 text-2xl hover:bg-sky-700 hover:cursor-pointer md:text-3xl lg:text-4xl"><ion-icon name="logo-discord"></ion-icon> Přihlásit se!</a>	
-		</header>
-		<footer class="m-auto text-center mt-5 text-lg">
-			<a class="hover:text-blue-700 hover:cursor-pointer"><ion-icon  name="logo-discord"></ion-icon></a>
-			<a class="hover:text-blue-400 hover:cursor-pointer"><ion-icon name="logo-twitter"></ion-icon></a>
-			<a class="hover:text-purple-500 hover:cursor-pointer"><ion-icon name="logo-twitch"></ion-icon></a>
-			<a class="hover:text-black hover:cursor-pointer"><ion-icon name="logo-tiktok"></ion-icon></a>
-			<a class="hover:text-blue-700 hover:cursor-pointer"><ion-icon name="logo-facebook"></ion-icon></a>
-			<a class="hover:text-pink-600 hover:cursor-pointer"><ion-icon name="logo-instagram"></ion-icon></a>
-			<a class="hover:text-red-500 hover:cursor-pointer"><ion-icon name="logo-youtube"></ion-icon></a>
-		</footer>
-	</section>
+<body class="bg-[<?=$system->SystemSettings["bg_page"]?>] font-mono ">
+
+	<?php
+    include("./inc/php/sections/header.php");
+	if(isset($_SESSION["msg"])){
+		echo $_SESSION["msg"];
+		unset($_SESSION["msg"]);
+	}
+
+		$module = '';
+		if(isset( $_GET["module"])){$module =  $_GET["module"];}
+		if(isset($_GET["save"])){
+			IncludePage($_GET["save"], $module, true);
+		}else if(isset($_GET["page"])){
+			IncludePage($_GET["page"], $module);
+		} else {
+			IncludePage("main");
+		}
+	?>
+	<?php include("./inc/php/sections/footer.php");?>
 </body>
 </html>
